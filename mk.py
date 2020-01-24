60aae7c771b795aa9004bfed69cd767e8de0fc18
mounikakaja <60178702+mounikakaja@users.noreply.github.com>
2020-01-24 17:33:37 +0530

    basic pyautogui done in class

---
+++ mk.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+pyautogui.position()
+pyautogui.click(28,160)
+time.sleep(2)
+pyautogui.typewrite("https://il53457.us-east-2.aws.snowflakecomputing.com/")
+pyautogui.typewrite(["ENTER"])
+
+
+
